110bfb2012005cc84046b799e1b44edb03f8e379
polina <polinaalexnder@gmail.com>
2019-12-27 19:03:35 +0300

    Added func

--- icode_onliner_catalog_integration/models/product_publicize_on_onliner.py
+++ icode_onliner_catalog_integration/models/product_publicize_on_onliner.py
@@ -9,7 +9,13 @@ class OnlinerCatalog(models.Model):
     _inherit = 'product.template'
 
     def publicize_on_onliner(self):
-        pass
+        return{
+            'type': 'ir.actions.act_window',
+            'res_model': 'res.partner',
+            'view_mode': 'form',
+            'views': [(self.env.ref('base.view_partner_form').id, 'form')],
+            'target': 'new'}
+
     #     summary_url = 'https://b2bapi.onliner.by/positions/{}?access-token={}'
     #
     #     token = self.env['ir.config_parameter'].sudo().get_param('icode_onliner_by_integration.token', default='')
@@ -32,36 +38,37 @@ class OnlinerCatalog(models.Model):
     #         print(d)
     #         self.id
     #
-    # def get_product_info(self):
-    #
-    # def form_dict(self):
-    #     exporter_dict = {
-    #         "id": 1,
-    #         "category": "MP3-плееры",
-    #         "vendor": "Apple",
-    #         "model": "iPod nano 16Gb (7th generation)",
-    #         "price": "20.00",
-    #         "currency": "BYN",
-    #         "comment": "Ваш комментарий",
-    #         "producer": "Foxconn,No.2,2nd Donghuan Road,10th Yousong Industrial District",
-    #         "importer": "ООО Музтрейд, г.Минск, ул. Кропоткина, 12\r\nООО Плеерсервис, г.Гомель, ул. Платонова, 16",
-    #         "serviceCenters": "ООО Музсервис, г.Минск, ул. П. Бровки, 5\r\nООО Плеерсервис, г.Гомель, ул. Платонова, 16",
-    #         "warranty": "12",
-    #         "deliveryTownTime": 1,
-    #         "deliveryTownPrice": "1.00",
-    #         "deliveryCountryTime": 5,
-    #         "deliveryCountryPrice": "2.00",
-    #         "productLifeTime": 36,
-    #         "isCashless": "нет",
-    #         "isCredit": "нет",
-    #         "stockStatus": "in_stock",
-    #         "courierDeliveryPrices": {
-    #             "region-1": {
-    #                 "type": "custom",
-    #                 "amount": "2.99"
-    #             },
-    #             "region-2": {
-    #                 "type": "no"
-    #             }
-    #         }
-    #     }
+        # def get_product_info(self):
+        #     active_ids = self.ids
+        #     for active_id in active_ids:
+        #
+        #         exporter_dict = {
+        #             "id": 1,
+        #             "category": "MP3-плееры",
+        #             "vendor": "Apple",
+        #             "model": "iPod nano 16Gb (7th generation)",
+        #             "price": "20.00",
+        #             "currency": "BYN",
+        #             "comment": "Ваш комментарий",
+        #             "producer": "Foxconn,No.2,2nd Donghuan Road,10th Yousong Industrial District",
+        #             "importer": "ООО Музтрейд, г.Минск, ул. Кропоткина, 12\r\nООО Плеерсервис, г.Гомель, ул. Платонова, 16",
+        #             "serviceCenters": "ООО Музсервис, г.Минск, ул. П. Бровки, 5\r\nООО Плеерсервис, г.Гомель, ул. Платонова, 16",
+        #             "warranty": "12",
+        #             "deliveryTownTime": 1,
+        #             "deliveryTownPrice": "1.00",
+        #             "deliveryCountryTime": 5,
+        #             "deliveryCountryPrice": "2.00",
+        #             "productLifeTime": 36,
+        #             "isCashless": "нет",
+        #             "isCredit": "нет",
+        #             "stockStatus": "in_stock",
+        #             "courierDeliveryPrices": {
+        #                 "region-1": {
+        #                     "type": "custom",
+        #                     "amount": "2.99"
+        #                 },
+        #                 "region-2": {
+        #                     "type": "no"
+        #                 }
+        #             }
+        #         }
