070cab26c34436803b63216fa28ba9e57a9e1e1d
polina <polinaalexnder@gmail.com>
2020-01-08 12:57:26 +0300

    Added wizard structure

--- icode_onliner_catalog_integration/wizards/product_template_get_product_info_wizard.py
+++ icode_onliner_catalog_integration/wizards/product_template_get_product_info_wizard.py
@@ -33,68 +33,9 @@ class ProductTemplateGetProductInfoWizard(models.TransientModel):
     courierDeliveryPrices = fields.Selection()
 
     def get_product_info(self):
-        product_ids = self.env['product.template'].browse(self._context.get('active_ids'))
-        if len(product_ids) > 1:
-            pass
+        active_id = self._context.get('active_id')
+        active_ids = self._context.get('active_ids', [active_id] if active_id else [])
+        for active_id in active_ids:
+            self.product_id = self.env['product.template'].search([('product_id', 'in', 'active_ids')]).id
+            self.product_category = self.env
 
-    # "id": 1,
-
-    #         "category": "MP3-плееры",
-    #         "vendor": "Apple",
-    #         "model": "iPod nano 16Gb (7th generation)",
-    #         "price": "20.00",
-    #         "currency": "BYN",
-    #         "comment": "Ваш комментарий",
-    #         "producer": "Foxconn,No.2,2nd Donghuan Road,10th Yousong Industrial District",
-    #         "importer": "ООО Музтрейд, г.Минск, ул. Кропоткина, 12\r\nООО Плеерсервис, г.Гомель, ул. Платонова, 16",
-    #         "serviceCenters": "ООО Музсервис, г.Минск, ул. П. Бровки, 5\r\nООО Плеерсервис, г.Гомель, ул. Платонова, 16",
-    #         "warranty": "12",
-    #         "deliveryTownTime": 1,
-    #         "deliveryTownPrice": "1.00",
-    #         "deliveryCountryTime": 5,
-    #         "deliveryCountryPrice": "2.00",
-    #         "productLifeTime": 36,
-    #         "isCashless": "нет",
-    #         "isCredit": "нет",
-    #         "stockStatus": "in_stock",
-    #         "courierDeliveryPrices": {
-    #             "region-1": {
-    #                 "type": "custom",
-    #                 "amount": "2.99"
-    #             },
-    #             "region-2": {
-    #                 "type": "no"
-    #             }
-    #         }
-    #     }
-
-    # def onclick_select_publichase_on_onliner_button(self):
-    #     self.ensure_one()
-    #     ResPartner = self.env['res.partner']
-    #     partner_id = ResPartner.search([('id', '=', self.partner_id.id)], limit=1)
-    #     vals = dict(
-    #         # country=self.country,
-    #         city=self.city,
-    #         country_id=self.country_id.id,
-    #         email=self.email,
-    #         legal_address=self.legal_address,
-    #         name=self.name,
-    #         phone=self.phone,
-    #         street2=self.street2,
-    #         street=self.street,
-    #         vat=self.vat,
-    #         website=self.website,
-    #     )
-    #     vals = {k: v for k, v in vals.items() if v}
-    #     partner_id.write(vals)
-    #     view = self.env.ref('base.view_partner_form')
-    #     return {
-    #         # 'name': 'Task created',
-    #         'view_type': 'form',
-    #         'view_mode': 'form',
-    #         'view_id': view.id,
-    #         'res_model': 'res.partner',
-    #         'type': 'ir.actions.act_window',
-    #         'res_id': partner_id.id,
-    #         'context': self.env.context
-    #     }
